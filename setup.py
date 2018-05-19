2fce7ea3b4d9ce3ea761c6549fb0a3e343233772
wangweichen <weichen.wang@jsxfedu.com>
2018-05-19 16:38:32 +0800

    revert

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@
 import glob
 
 from setuptools import setup, Extension
-from setuptools.command import install_lib
+from setuptools.command import install_lib as _install_lib
+
 
 # # Patch "install_lib" command to run build_clib before build_ext
 # # to properly work with easy_install.
 # # See: http://bugs.python.org/issue5243
-# class install_lib(_install_lib.install_lib):
-#     def build(self):
-#         if not self.skip_build:
-#             if self.distribution.has_pure_modules():
-#                 self.run_command('build_py')
-#                 if self.distribution.has_c_libraries():
-#                     self.run_command('build_clib')
-#                 if self.distribution.has_ext_modules():
-#                     self.run_command('build_ext')
+class install_lib(_install_lib.install_lib):
+    def build(self):
+        if not self.skip_build:
+            if self.distribution.has_pure_modules():
+                self.run_command('build_py')
+                if self.distribution.has_c_libraries():
+                    self.run_command('build_clib')
+                if self.distribution.has_ext_modules():
+                    self.run_command('build_ext')
 
 
 # To link the extension with the C library, distutils passes the "-lLIBRARY"
